6d303c347c3ac39b8a6a1c1cedb5308c15681e61
vargaadam23 <vargaadam23@yahoo.com>
2023-08-12 18:26:40 +0300

    Initial project setup

---
+++ src/main/java/com/tsg/apartments/ApartmentsApplication.java
@@ -0,0 +1,13 @@
+package com.tsg.apartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsApplication.class, args);
+	}
+
+}
